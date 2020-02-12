207d81a6eb02cdca4017602405a61aa0bfb8931f
jtjohn24 <jtjohn24@cs.washington.edu>
2020-02-11 17:27:19 -0800

    Add clone to slice

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Error;
 
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Ord, PartialOrd, Eq)]
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Ord, PartialOrd, Eq, Copy)]
 pub struct Slice {
     pub start: u64, // inclusive
     pub end: u64,   // inclusive
